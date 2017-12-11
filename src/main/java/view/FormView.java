14a06f8e0e21a3bc011855aea3cd9f51ae107a24
EmilTomellini <tsumi.on@gmail.com>
2017-12-11 19:02:21 +0100

    update

---
+++ src/main/java/view/FormView.java
@@ -0,0 +1,28 @@
+package view;
+
+import com.vaadin.ui.VerticalLayout;
+
+public class FormView extends VerticalLayout{
+
+	private PatientView pv = new PatientView();
+	
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	private class PatientView extends VerticalLayout{
+		
+		public PatientView() {
+			
+		}
+		
+		
+	}
+	
+}
